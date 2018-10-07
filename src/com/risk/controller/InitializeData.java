d30df02e36d0935a1cb0d1c8dbe272990655bfff
Himen Sidhpura <himens72@gmail.com>
2018-10-07 04:53:13 -0400

    Declare Reinforcement Phase in InitializeData.java
    Signed-off-by: Himen Sidhpura <himens72@gmail.com>

--- src/com/risk/controller/InitializeData.java
+++ src/com/risk/controller/InitializeData.java
@@ -3,6 +3,10 @@ package com.risk.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.risk.models.Continent;
+import com.risk.models.Players;
+import com.risk.models.Territory;
+
 
 public class InitializeData {
 	
@@ -10,27 +14,25 @@ public class InitializeData {
 	int playerCount;
 
 	int armies;
-	ArrayList<String> currentPlayers;
 	HashMap<String, ArrayList<String>> contTerr; 						//for storing [Continents] and it's [Territories].
 	HashMap<String, Integer> contValue; 								//for storing [Continents] and it's [Winning Value].
 	HashMap<String, ArrayList<String>> adjcentTerr;						//for storing [Territories] and it's [Adjacent Territories].
+	Continent continent;
+	Territory territory;
+	Players players;
 	
-	public InitializeData(String filePath, int playerCount, int armies, ArrayList<String> currentPlayers) {
+	
+	public InitializeData(String filePath, int playerCount, int armies, Players players) {
 		this.filePath = filePath;
 		this.playerCount = playerCount;
 		this.armies = armies;
-		this.currentPlayers = currentPlayers;
+		this.players = players;
 		System.out.println("FilePath ------------- : "+filePath);
 		System.out.println("Player Playing ------------- : "+playerCount);
 		System.out.println("Armies for Each Player ------------- : "+armies);
-		System.out.println("No. of Player ------------- : "+currentPlayers);
+		System.out.println("No. of Player ------------- : "+players.getPlayerPlaying());
 	}
-	public ArrayList<String> getCurrentPlayers() {
-		return currentPlayers;
-	}
-	public void setCurrentPlayers(ArrayList<String> currentPlayers) {
-		this.currentPlayers = currentPlayers;
-	}
+
 	public int getArmies() {
 		return armies;
 	}
@@ -69,13 +71,52 @@ public class InitializeData {
 	}
 	public boolean generateData(){
 		BoardData boardData = new BoardData(filePath);
-		boardData.generateBoardData(filePath);
-		contTerr = boardData.contTerr;
-		contValue = boardData.contValue;
-		adjcentTerr = boardData.adjcentTerr;
-		Reinforcement reinforcement = new Reinforcement(currentPlayers,contTerr,contValue,adjcentTerr);
-		
-		return false;
+		boardData.generateBoardData();
+		System.out.println("Model Data Continent Value: - " + boardData.continentObject.getContinentValue());
+		System.out.println("Model Data Territory with Adjacent: - " + boardData.territoryObject.getAdjacentTerritory());
+		System.out.println("Model Data Territory with Adjacent Size: - " + boardData.territoryObject.getAdjacentTerritory().size());
+		System.out.println("Model Data Continent with Territory: - " + boardData.continentObject.getContinentTerritory());
+		System.out.println("Model Data of Territory List : - " + boardData.territoryObject.getTerritoryList());
+		System.out.println("Model Data of Territory List Size : - " + boardData.territoryObject.getTerritoryList().size());
+		System.out.println("Model Data of Territory with Continents List : - " + boardData.territoryObject.getTerritoryCont());
+		System.out.println("Model Data of Territory with Continents List Size : - " + boardData.territoryObject.getTerritoryCont().size());
+		continent = boardData.continentObject;
+		territory = boardData.territoryObject;
+		players.selectPlayers(players.getPlayerList(), playerCount);
+		Reinforcement reinforcement = new Reinforcement(players,continent,territory);
+		reinforcement.initialReinforcement();
+		setContinent(reinforcement.continent);
+		setPlayers(reinforcement.players);
+		setTerritory(reinforcement.territory);
+
+		//System.out.println("Territory After Initial Reinforcement User" + territory.getTerritoryUser());
+		//System.out.println("Territory After Initial Reinforcement Army" + territory.getTerritoryArmy());
+		//System.out.println("-->" + players.getPlayerContinent("Manan").getContinentOwnedterritory());
+		return true;
+	}
+
+	public Continent getContinent() {
+		return continent;
+	}
+
+	public void setContinent(Continent continent) {
+		this.continent = continent;
+	}
+
+	public Territory getTerritory() {
+		return territory;
+	}
+
+	public void setTerritory(Territory territory) {
+		this.territory = territory;
+	}
+
+	public Players getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(Players players) {
+		this.players = players;
 	}
 	
 	
