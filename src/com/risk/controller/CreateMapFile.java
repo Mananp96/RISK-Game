180f2b409562ffb68d13bc7d2b0b1a7a259c6bc2
Khyati389 <khyati.chaudhary1996@gmail.com>
2018-10-15 18:22:41 -0400

    Javadoc added.

--- src/com/risk/controller/CreateMapFile.java
+++ src/com/risk/controller/CreateMapFile.java
@@ -6,56 +6,55 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * this class is used to CREATE a map file from scratch.
- *
+ * This class is used to <b>create a new map file</b> from scratch.
  */
 public class CreateMapFile {
-	
+
 	String mapData;
 	String fileName = "mapFile.map";
 	BufferedWriter bufferedWriter = null;
 	FileWriter fileWriter = null;
-	
-	public CreateMapFile(String mapData) {
-		this.mapData = mapData;
+
+	/**
+	 * Set the mapData.
+	 * @param newMapData User entered Input to create a map file.
+	 */
+	public CreateMapFile(String newMapData) {
+		this.mapData = newMapData;
 	}
 
 	/**
-	 * If file gets created then the createNewFile() 
-	 * method would return true or if the file is 
-	 * already present it would return false.
-	 * 
-	 * 
+	 * This method is used to create a new File and write the mapData (User input to create a new map file)
+	 * to file. checks if newly created file has valid map data or not? 
 	 */
 	public void createMap() {
-	    try {
-	    	
-	    	File file = new File(fileName); 
-	        boolean isFileCreated = file.createNewFile();
-	        
-	        if(isFileCreated) {
-	            fileWriter = new FileWriter(file);
-	            bufferedWriter = new BufferedWriter(fileWriter);
-	            bufferedWriter.write(mapData);
-	            bufferedWriter.flush();
-	            bufferedWriter.close();
-	           
-	            BoardData boardData = new BoardData(fileName);
-	            boolean isMapValid = boardData.generateBoardData();
-	            
-	            if(isMapValid) {
-	            	System.out.println("Map is Valid.");
-	            	System.out.println("File has been created successfully");
-	            } else {
-	            	System.out.println("map is not valid.");
-	            }
-	            
-	        } else {
-	            System.out.println("File already present at the specified location");
-	       	}
-	        
-        } catch (IOException exception) {
-       		exception.printStackTrace();
-	    }       
+		
+		try {
+			File file = new File(fileName); 
+			boolean isFileCreated = file.createNewFile();
+
+			if(isFileCreated) {
+				fileWriter = new FileWriter(file);
+				bufferedWriter = new BufferedWriter(fileWriter);
+				bufferedWriter.write(mapData);
+				bufferedWriter.flush();
+				bufferedWriter.close();
+				BoardData boardData = new BoardData(fileName);
+				boolean isMapValid = boardData.generateBoardData();
+
+				if(isMapValid) {
+					System.out.println("Map is Valid.");
+					System.out.println("File has been created successfully");
+				} else {
+					System.out.println("map is not valid.");
+				}
+
+			} else {
+				System.out.println("File already present at the specified location");
+			}
+
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}       
 	} 
 }
