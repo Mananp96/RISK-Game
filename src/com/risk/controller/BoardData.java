24750290141042e48215c057e902e76b4c66ffec
Khyati389 <khyati.chaudhary1996@gmail.com>
2018-10-15 18:01:53 -0400

    added Javadoc and removed some unnecessary code.

--- src/com/risk/controller/BoardData.java
+++ src/com/risk/controller/BoardData.java
@@ -3,126 +3,113 @@ package com.risk.controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 import com.risk.exception.InvalidMapException;
 import com.risk.models.Continent;
 import com.risk.models.Territory;
 import com.risk.validate.MapValidator;
 
+/**
+ * This class is used to <b>Parse map file</b>, <b>Generate Data</b> 
+ * and send the request to <b>validate map file</b>. 
+ *
+ */
 public class BoardData {
 
 	private String filePath;
-	StringBuilder stringBuilder;
-	boolean contFlag = false; 	//Continents flag
-	boolean terrFlag = false;	//Territories flag
+
+	boolean continentFlag = false; 	
+	boolean territoriesFlag = false;
 	boolean isMapValid = false;
-	
+
 	String[] continentsArray;
 	String[] territoriesArray;
-	
-	ArrayList<String> continentsList;
-	ArrayList<String> territoriesList;
 
-	HashMap<String, ArrayList<String>> contTerr; 						//for storing [Continents] and it's [Territories].
-	HashMap<String, Integer> contValue; 								//for storing [Continents] and it's [Winning Value].
-	HashMap<String, ArrayList<String>> adjcentTerr;						//for storing [Territories] and it's [Adjacent Territories].
-	HashMap<String, HashMap<String, ArrayList<String>>> userOwnedTerr;	//for storing [User] and it's owned [Continents] [Territories].
-	HashMap<String, String> territories;
-	HashMap<String, String> adjacentTerritories;
-	
-	private BufferedReader reader;
-	
+	BufferedReader reader;
+
 	public Continent continentObject;
 	public Territory territoryObject;
-	
+
+	/**
+	 * This constructor set the User specified path of map File.
+	 * @param filePath path of map File.
+	 */
 	public BoardData(String filePath) {
 		super();
 		this.filePath = filePath;
 	}
+
+	/**
+	 * This method is used to Read and Generate Data from map file,
+	 * set the data to Continent and Territory Model, and
+	 * calls mapValidate() method to check if Generated Data is Correct or not?.
+	 * 
+	 * @return true if map data is valid, else return false.
+	 */
 	public boolean generateBoardData() {
 
 		continentObject = new Continent();
 		territoryObject = new Territory();
 		String currentLine;
-		contValue = new HashMap<>();
-		contTerr = new HashMap<>();
-		adjcentTerr = new HashMap<>();
-		
 		try 
 		{
 			reader = new BufferedReader(new FileReader(filePath));
-			System.out.println(filePath);
-			stringBuilder = new StringBuilder();
-			
-			while((currentLine = reader.readLine()) != null) {		
+			while((currentLine = reader.readLine()) != null) {	
+
 				if(currentLine.equals("[Continents]")) {
-					
-					contFlag = true;
-					terrFlag = false;
+					continentFlag = true;
+					territoriesFlag = false;
 				}
 				if(currentLine.equals("[Territories]")) {
-					
-					terrFlag = true;
-					contFlag = false;
+					territoriesFlag = true;
+					continentFlag = false;
 				}
-				
+
 				//assign Continents List.
-				if(contFlag && ! (currentLine.isEmpty()) && ! currentLine.equalsIgnoreCase("[Continents]")) {
-					
+				if(continentFlag && ! (currentLine.isEmpty()) && ! currentLine.equalsIgnoreCase("[Continents]")) {
+
 					continentsArray = currentLine.split("=");
 					continentObject.setContinentValue(continentsArray[0], Integer.parseInt(continentsArray[1]));
-					
+
 				}
-				
+
 				//assign Territories List.
-				if(terrFlag && ! (currentLine.isEmpty() || currentLine.equals("[Territories]"))) {
-					territoriesList = new ArrayList<>(); 
+				if(territoriesFlag && ! (currentLine.isEmpty() || currentLine.equals("[Territories]"))) {
 					territoriesArray = currentLine.split(",");
-					
 					//to assign Adjacent Territories HashMap
 					for(int i = 4; i < territoriesArray.length ; i++) {
-						territoryObject.addAdjacentTerritory(territoriesArray[0], territoriesArray[i]);
-//						if(territoryObject.isFlag()) {
-//							break;
-//						}
-//						territoryObject.addAdjacentTerritoriesMap(territoriesArray[i],territoriesArray[i]);
+						territoryObject.addAdjacentTerritory(territoriesArray[0], territoriesArray[i]);						
 					}
 					territoryObject.addTerritory(territoriesArray[0]);
-//					territoryObject.addTerritoriesMap(territoriesArray[0], territoriesArray[0]);
-//					if(territoryObject.isFlag()) {
-//						break;
-//					}
 					territoryObject.addTerritoryCont(territoriesArray[0], territoriesArray[3]);
 					continentObject.addContinentTerritory(territoriesArray[3], territoriesArray[0]);
 				}
-				
+
 			}
-			
-//			if(!territoryObject.isFlag()) {
-				MapValidator mapValidator = new MapValidator(continentObject,territoryObject);
-				isMapValid = mapValidator.validateMap();
-//			}else {
-//				return false;
-//			}
-			
-			
-		reader.close();
+
+			MapValidator mapValidator = new MapValidator(continentObject,territoryObject);
+			isMapValid = mapValidator.validateMap();
+			reader.close();
 		}catch(IOException | InvalidMapException e) {
 			e.printStackTrace();
 		}
-		
-//		System.out.println("isMapValid--------------"+isMapValid);
+
 		return isMapValid;
-		
+
 	}
-	
-	
+
+	/**
+	 * This method returns the path of map File.
+	 * @return filePath path of map File.
+	 */
 	public String getFilePath() {
 		return filePath;
 	}
 
+	/**
+	 * This method sets the filePath.
+	 * @param filePath path of map File.
+	 */
 	public void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}
