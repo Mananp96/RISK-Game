582c0a019a6dc7ec7daf0922c07a5e5ef2fbfc11
Khyati389 <khyati.chaudhary1996@gmail.com>
2018-10-15 19:50:09 -0400

    added Javadoc

--- src/com/risk/controller/InitializeData.java
+++ src/com/risk/controller/InitializeData.java
@@ -4,17 +4,26 @@ import com.risk.models.Continent;
 import com.risk.models.Players;
 import com.risk.models.Territory;
 
-
+/**
+ * This class is used to invoke Initial Reinforcement process if data of map file is valid.
+ * 
+ */
 public class InitializeData {
-	
+
 	String filePath;
 	int playerCount;
 	int armies;
 	Continent continent;
 	Territory territory;
 	Players players;
-	
-	
+
+	/**
+	 * This constructor is used to set the data members of class.
+	 * @param filePath	path of map File.
+	 * @param playerCount	Number of Players in game.
+	 * @param armies	Number of armies per Player.
+	 * @param players	model object of Players.
+	 */
 	public InitializeData(String filePath, int playerCount, int armies, Players players) {
 		this.filePath = filePath;
 		this.playerCount = playerCount;
@@ -22,58 +31,117 @@ public class InitializeData {
 		this.players = players;
 	}
 
+	/**
+	 * This method returns the variable armies.
+	 * @return Number of armies per player.
+	 */
 	public int getArmies() {
 		return armies;
 	}
+
+	/**
+	 * Set the armies.
+	 * @param armies Number of armies per player.
+	 */
 	public void setArmies(int armies) {
 		this.armies = armies;
 	}
+
+	/**
+	 * return filePath.
+	 * @return path of map File.
+	 */
 	public String getFilePath() {
 		return filePath;
 	}
+
+	/**
+	 * Set filePath.
+	 * @param filePath path of map File.
+	 */
 	public void setFilePath(String filePath) {
 		this.filePath = filePath;
 	}
+
+	/**
+	 * Return playerCount.
+	 * @return Number of Players in game.
+	 */
 	public int getPlayerCount() {
 		return playerCount;
 	}
+
+	/**
+	 * Set playerCount.
+	 * @param playerCount Number of Players in game.
+	 */
 	public void setPlayerCount(int playerCount) {
 		this.playerCount = playerCount;
 	}
-	
+
+	/**
+	 * Return object continent.
+	 * @return a object of Model Continent.
+	 */
 	public Continent getContinent() {
 		return continent;
 	}
 
+	/**
+	 * Set the object continent.
+	 * @param continent object of Model Continent.
+	 */
 	public void setContinent(Continent continent) {
 		this.continent = continent;
 	}
 
+	/**
+	 * Return the object territory of Model Territory.
+	 * @return a object territory of Model Territory.
+	 */
 	public Territory getTerritory() {
 		return territory;
 	}
 
+	/**
+	 * Set the object territory of Model Territory.
+	 * @param territory object of Model Territory.
+	 */
 	public void setTerritory(Territory territory) {
 		this.territory = territory;
 	}
 
+	/**
+	 * Return the object players of Model Players.
+	 * @return a object players of Model Players.
+	 */
 	public Players getPlayers() {
 		return players;
 	}
 
+	/**
+	 * Set the object players of Model Players.
+	 * @param players object of Model Players
+	 */
 	public void setPlayers(Players players) {
 		this.players = players;
 	}
-	
-	/*
-	 * this method is used to generate and validate the data of .map file.
-	 * return true if data isValid or else return false.
+
+	/**
+	 * This method gives the path of map file to BoardData class, invoke the method 
+	 * {@link BoardData#generateBoardData()} of class BoardData
+	 * for the purpose of Generate Data and Validate map Data from map file. 
+	 * if map is valid then it starts the process of Initial Reinforcement: invoke
+	 * the method {@linkplain Reinforcement#initialReinforcement()} of class Reinforcement.
+	 * 
+	 * 
+	 * @return true if map Data is Valid else, return false.
 	 */
 	public boolean generateData(){
-		
+
 		BoardData boardData = new BoardData(filePath);
 		boolean isMapValid = boardData.generateBoardData();
-		
+
 		System.out.println("Model Data Continent Value: - " + boardData.continentObject.getContinentValue());
 		System.out.println("Model Data Territory with Adjacent: - " + boardData.territoryObject.getAdjacentTerritory());
 		System.out.println("Model Data Territory with Adjacent Size: - " + boardData.territoryObject.getAdjacentTerritory().size());
@@ -82,7 +150,7 @@ public class InitializeData {
 		System.out.println("Model Data of Territory List Size : - " + boardData.territoryObject.getTerritoryList().size());
 		System.out.println("Model Data of Territory with Continents List : - " + boardData.territoryObject.getTerritoryCont());
 		System.out.println("Model Data of Territory with Continents List Size : - " + boardData.territoryObject.getTerritoryCont().size());
-			
+
 		continent = boardData.continentObject;
 		territory = boardData.territoryObject;
 		players.selectPlayers(players.getPlayerList(), playerCount);
--- src/com/risk/controller/Reinforcement.java
+++ src/com/risk/controller/Reinforcement.java
@@ -9,25 +9,36 @@ import com.risk.models.Players;
 import com.risk.models.Territory;
 
 public class Reinforcement {
-	
+
 	Players players;
 	Continent continent;
 	Territory territory;
 	ArmiesSelection armies;
-	
+
+	/**
+	 * This constructor is used to set the model object continent, players, territory.
+	 * @param players object of model Players.
+	 * @param continent object of model Continent.
+	 * @param territory object of model Territory.
+	 */
 	public Reinforcement(Players players, Continent continent, Territory territory) {
-		// TODO Auto-generated constructor stub
 		this.continent = continent;
 		this.territory = territory;
 		this.players = players;
 	}
 
-
+	/**
+	 * This method is used to start initial Reinforcement process:
+	 * Players are allocated a number of initial armies, depending on the number of players.
+	 * round-robin fashion, the players place their given armies one by one on their own countries. 
+	 * 
+	 */
 	public void initialReinforcement() {
 		int playerSize = players.getPlayerList().size();
 		armies = new ArmiesSelection(playerSize);
 		System.out.println("Initial Reinforcement Started");
 		System.out.println("Player in Game : " +playerSize);
+
 		for(int i = 0 ; i < playerSize ; i++ ) {
 			String playerName = players.getPlayerList().get(i);
 			players.addPlayerContinent(playerName,new Continent());
@@ -35,28 +46,29 @@ public class Reinforcement {
 			System.out.println(playerName +" Assigned with Continent Object");
 			System.out.println(playerName +" Assigned with Initial Army of " + players.getPlayerArmy(playerName));		
 		}
+		
 		Map<String, String> territoryContinent = territory.getTerritoryCont();
 		int playerCount = 0;
 		for(int i=0;i< territory.getTerritoryList().size();i++) {
-        	    	String playerName = players.getPlayerList().get(playerCount);
-        			// Retrieve Random Territory
-        		Object randomTerritory = territoryContinent.keySet().toArray()[new Random().nextInt(territoryContinent.keySet().toArray().length)];
-        	    	if(players.getPlayerArmy(playerName) >= 1) {
-        		    Continent tempContinent = players.getPlayerContinent(playerName);
-        		    tempContinent.addContinentOwnedTerritory(territoryContinent.get(randomTerritory),randomTerritory.toString(), true); 	
-        		    territoryContinent.remove(randomTerritory);
-        		    territory.updateTerritoryArmy(randomTerritory.toString(), 1, "ADD");
-        		    territory.updateTerritoryUser(playerName, randomTerritory.toString());
-        		    players.updateArmy(playerName, 1, "Remove");
-        		    players.updatePlayerContinent(playerName, tempContinent);
-        		    playerCount++;
-        		    if(playerCount >= playerSize)
-        		    		playerCount = 0;
-        		} else {
-        		    territory.updateTerritoryArmy(randomTerritory.toString(), 0, "ADD");
-        		    territory.updateTerritoryUser("No Player", randomTerritory.toString());
-        		}
+			String playerName = players.getPlayerList().get(playerCount);
+			// Retrieve Random Territory
+			Object randomTerritory = territoryContinent.keySet().toArray()[new Random().nextInt(territoryContinent.keySet().toArray().length)];
+			if(players.getPlayerArmy(playerName) >= 1) {
+				Continent tempContinent = players.getPlayerContinent(playerName);
+				tempContinent.addContinentOwnedTerritory(territoryContinent.get(randomTerritory),randomTerritory.toString(), true); 	
+				territoryContinent.remove(randomTerritory);
+				territory.updateTerritoryArmy(randomTerritory.toString(), 1, "ADD");
+				territory.updateTerritoryUser(playerName, randomTerritory.toString());
+				players.updateArmy(playerName, 1, "Remove");
+				players.updatePlayerContinent(playerName, tempContinent);
+				playerCount++;
+				if(playerCount >= playerSize)
+					playerCount = 0;
+			} else {
+				territory.updateTerritoryArmy(randomTerritory.toString(), 0, "ADD");
+				territory.updateTerritoryUser("No Player", randomTerritory.toString());
+			}
 		}
-				
+
 	}
 }
