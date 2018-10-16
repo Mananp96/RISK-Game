04e99ae63c6bf97bc5e5f8dc789da64a35ec3d13
shalinpatel610 <shalinpatel610@gmail.com>
2018-10-16 05:25:00 -0400

    Store Continent and Territory Data read from file

--- src/com/risk/controller/BoardData.java
+++ src/com/risk/controller/BoardData.java
@@ -24,12 +24,42 @@ public class BoardData {
 
 	String[] continentsArray;
 	String[] territoriesArray;
+	StringBuilder continentData;
 
+	StringBuilder territoryData;
 	BufferedReader reader;
 
 	public Continent continentObject;
 	public Territory territoryObject;
 
+	/**
+	 * Get Continent Data from file
+	 * @return continentData
+	 */
+	public StringBuilder getContinentData() {
+	    return continentData;
+	}
+	/**
+	 * set Continent Data from file
+	 * @param continentData all continent Data
+	 */
+	public void setContinentData(StringBuilder continentData) {
+	    this.continentData = continentData;
+	}
+	/**
+	 * Get Territory Data from file
+	 * @return territoryData
+	 */
+	public StringBuilder getTerritoryData() {
+	    return territoryData;
+	}
+	/**
+	 * Set Territory Data from File
+	 * @param territoryData all territory data
+	 */
+	public void setTerritoryData(StringBuilder territoryData) {
+	    this.territoryData = territoryData;
+	}
 	/**
 	 * This constructor set the User specified path of map File.
 	 * @param filePath path of map File.
@@ -47,7 +77,8 @@ public class BoardData {
 	 * @return true if map data is valid, else return false.
 	 */
 	public boolean generateBoardData() {
-
+	    	continentData = new StringBuilder();
+	    	territoryData = new StringBuilder();
 		continentObject = new Continent();
 		territoryObject = new Territory();
 		String currentLine;
@@ -67,7 +98,7 @@ public class BoardData {
 
 				//assign Continents List.
 				if(continentFlag && ! (currentLine.isEmpty()) && ! currentLine.equalsIgnoreCase("[Continents]")) {
-
+				    	continentData.append(currentLine+"\n");
 					continentsArray = currentLine.split("=");
 					continentObject.setContinentValue(continentsArray[0], Integer.parseInt(continentsArray[1]));
 
@@ -75,6 +106,7 @@ public class BoardData {
 
 				//assign Territories List.
 				if(territoriesFlag && ! (currentLine.isEmpty() || currentLine.equals("[Territories]"))) {
+				    	territoryData.append(currentLine +"\n");
 					territoriesArray = currentLine.split(",");
 					//to assign Adjacent Territories HashMap
 					for(int i = 4; i < territoriesArray.length ; i++) {
@@ -93,9 +125,7 @@ public class BoardData {
 		}catch(IOException | InvalidMapException e) {
 			e.printStackTrace();
 		}
-
 		return isMapValid;
-
 	}
 
 	/**
